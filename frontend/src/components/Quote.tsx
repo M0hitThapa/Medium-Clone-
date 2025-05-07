ea48477e08d554d5f4b74906eb0233cc6758ef69
Mohit Thapa <thapamohit2058@gmail.com>
2025-05-07 23:00:50 +0545

    add routes and layout for signup page

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,24 @@
+
+
+const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+       <div className="flex justify-center">
+       <div className="max-w-lg  ">
+      <div className="text-2xl font-black">
+      "Every story starts with a single step — or in this case, a sign up. Join a community where your thoughts, ideas, and voice can spark something bigger."
+      </div>
+      <div className="max-w-md  text-lg font-semibold mt-4">
+        Henry Horrid
+       </div>
+       <div className="max-w-md  text-sm font-light text-slate-400">
+        CEO
+       </div>
+        </div>
+       </div>
+      
+    </div>
+  )
+}
+
+export default Quote
