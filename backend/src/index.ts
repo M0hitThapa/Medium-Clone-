9bd06895b83a50c6a63289c1b5028eae5832181b
Mohit Thapa <thapamohit2058@gmail.com>
2025-05-09 00:02:15 +0545

    connect signin and signup component with backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-
+import { cors } from 'hono/cors'
 import { userRouter } from './routes/user'
 import { blogRouter } from './routes/blog'
 
@@ -11,7 +11,7 @@ DATABASE_URL:string;
 JWT_SECRET:string;
   }
 }>()
-
+app.use('/*', cors())
 app.route("api/v1/user", userRouter)
 app.route("api/v1/blog", blogRouter)
 
--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,14 +1,29 @@
 
 import { useState, type ChangeEvent } from "react";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 import type { SignupInput } from "@thapamohit/medium-common";
+import { BACKEND_URL } from "../config";
 
 const Auth = ({type}: {type: "signup" | "signin"}) => {
+    const navigate = useNavigate();
     const [postInputs, setPostInputs] = useState<SignupInput>({
         name:"",
         username:"",
         password:""
-    })
+    });
+
+async function sendRequest() {
+try {
+    const response =  await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`, postInputs)
+    const jwt = response.data;
+    localStorage.setItem("token", jwt)
+    navigate("/blogs")
+} catch (e) {
+    console.log(e)
+}
+}
+
   return (
     <div className="h-screen flex  justify-center flex-col">
         
@@ -49,7 +64,7 @@ const Auth = ({type}: {type: "signup" | "signin"}) => {
             })
 
         }} />
-        <button type="button" className=" mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+        <button onClick={sendRequest} type="button" className=" mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
             {type === "signup" ? "Sign up": "sign in"}
         </button>
 
